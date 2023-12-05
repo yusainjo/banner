631917e3b0eb200eb550308a324f7a63e40773a1
yusainjo <152935480+yusainjo@users.noreply.github.com>
2023-12-06 06:00:59 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '56410e3c20fdad26a8b1fe6c4878f3fb',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/56410e3c20fdad26a8b1fe6c4878f3fb/invoke.js"></scr' + 'ipt>');
